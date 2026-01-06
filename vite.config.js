2e632047947b5e2bd4d0ac860ccfbd27a81e38de
Usman Kapadiya <hirenk@intellidt.com>
2026-01-06 15:49:26 +0530

    Vite update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base: './',
+    base: '/',
+  build: {
+    chunkSizeWarningLimit: 2500,
+  },
   server: {
-    port: 3000,
-    open: true
-  }
+            port: 3000,
+        },
 })
